8d3ba34f9802b7ea79b6802c6988b3d76f8dcd69
Ganesh Nomula <gnomula198@gmail.com>
2019-03-23 01:03:20 -0500

    Face recog change

--- src/components/Facerecognition/Facerecognition.js
+++ src/components/Facerecognition/Facerecognition.js
@@ -4,8 +4,8 @@ import './Facerecognition.css'
 var FaceRecognition = ({box, imageUrl}) => {
 	var leftCol = 0, rightCol = 0, topRow = 0, bottomRow = 0;
 	//console.log('box in Facerecognition', box);
-	console.log('this is the box array coming in from App.Js ',box)
 	for(var i =0; i < box.length; i++) {
+		console.log(box[i].right_Col)
 		leftCol = box[i].left_Col;
 		rightCol = box[i].right_Col;
 		topRow = box[i].top_Row;
@@ -18,7 +18,17 @@ var FaceRecognition = ({box, imageUrl}) => {
 		<div className='center ma'>
 			<div className='absolute mt3'>
 				<img id = 'input_image' alt='' src={imageUrl} width = '500px' height = 'auto'/>
-				<div className = 'bounding_box' style = {{top: topRow, right: rightCol, bottom: bottomRow, left: leftCol }}></div>
+				<div className='bounding-box'>
+					{box.map((index) => (
+							<div key={index} style={{
+								top:topRow,
+								right:rightCol,
+								bottom:bottomRow,
+								left:leftCol}}>
+							</div>
+						)
+					)}
+				</div>
 			</div>
 		</div>
 	);
@@ -29,15 +39,15 @@ export default FaceRecognition;
 
 
 
-// var leftCol = 0, rightCol = 0, topRow = 0, bottomRow = 0;
-// 	// for(var j = 3; j < box.length; j++) 
-//       //console.log('Im inside looop: j = ', j)
-//       var inside_box = box[0];
-//       //console.log('inside_box: ', inside_box)
-//       leftCol = inside_box.leftCol;
-//       topRow = inside_box.topRow;
-//       rightCol = inside_box.rightCol;
-//       bottomRow = inside_box.bottomRow;
-//       //console.log(leftCol, topRow, rightCol, bottomRow);
-//     // }
- 				
+	// var leftCol = 0, rightCol = 0, topRow = 0, bottomRow = 0;
+	// // for(var j = 3; j < box.length; j++) {
+ //      //console.log('Im inside looop: j = ', j)
+ //      var inside_box = box[0];
+ //      //console.log('inside_box: ', inside_box)
+ //      leftCol = inside_box.leftCol;
+ //      topRow = inside_box.topRow;
+ //      rightCol = inside_box.rightCol;
+ //      bottomRow = inside_box.bottomRow;
+ //      //console.log(leftCol, topRow, rightCol, bottomRow);
+ //    // }
+ 				// <div className = 'bounding_box' style = {{top: topRow, right: rightCol, bottom: bottomRow, left: leftCol }}>
